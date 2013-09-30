9094de7e3af8028fbf2b692b66a6c7db5aca9f8d
Jeff Standen <jeff@webgroupmedia.com>
2013-09-30 15:36:16 -0700

    * [Virtual Attendants] Implement custom Virtual Attendant behavior for JIRA project records.

---
+++ api/events/abstract_event_jira_project.php
@@ -0,0 +1,385 @@
+<?php
+/***********************************************************************
+| Cerb(tm) developed by Webgroup Media, LLC.
+|-----------------------------------------------------------------------
+| All source code & content (c) Copyright 2013, Webgroup Media LLC
+|   unless specifically noted otherwise.
+|
+| This source code is released under the Devblocks Public License.
+| The latest version of this license can be found here:
+| http://cerberusweb.com/license
+|
+| By using this software, you acknowledge having read this license
+| and agree to be bound thereby.
+| ______________________________________________________________________
+|	http://www.cerberusweb.com	  http://www.webgroupmedia.com/
+***********************************************************************/
+
+abstract class AbstractEvent_JiraProject extends Extension_DevblocksEvent {
+	protected $_event_id = null; // override
+
+	/**
+	 *
+	 * @param integer $project_id
+	 * @return Model_DevblocksEvent
+	 */
+	function generateSampleEventModel(Model_TriggerEvent $trigger, $project_id=null) {
+		
+		if(empty($project_id)) {
+			// Pull the latest record
+			list($results) = DAO_JiraProject::search(
+				array(),
+				array(
+					//new DevblocksSearchCriteria(SearchFields_Task::IS_CLOSED,'=',0),
+				),
+				10,
+				0,
+				SearchFields_JiraProject::ID,
+				false,
+				false
+			);
+			
+			shuffle($results);
+			
+			$result = array_shift($results);
+			
+			$project_id = $result[SearchFields_JiraProject::ID];
+		}
+		
+		return new Model_DevblocksEvent(
+			$this->_event_id,
+			array(
+				'project_id' => $project_id,
+			)
+		);
+	}
+	
+	function setEvent(Model_DevblocksEvent $event_model=null) {
+		$labels = array();
+		$values = array();
+
+		/**
+		 * Project
+		 */
+		
+		@$project_id = $event_model->params['project_id'];
+		$merge_labels = array();
+		$merge_values = array();
+		CerberusContexts::getContext('cerberusweb.contexts.jira.project', $project_id, $merge_labels, $merge_values, null, true);
+
+			// Merge
+			CerberusContexts::merge(
+				'project_',
+				'',
+				$merge_labels,
+				$merge_values,
+				$labels,
+				$values
+			);
+
+		/**
+		 * Return
+		 */
+
+		$this->setLabels($labels);
+		$this->setValues($values);
+	}
+	
+	function renderSimulatorTarget($trigger, $event_model) {
+		$context = 'cerberusweb.contexts.jira.project';
+		$context_id = $event_model->params['project_id'];
+		DevblocksEventHelper::renderSimulatorTarget($context, $context_id, $trigger, $event_model);
+	}
+	
+	function getValuesContexts($trigger) {
+		$vals = array(
+			'project_id' => array(
+				'label' => 'Project',
+				'context' => 'cerberusweb.contexts.jira.project',
+			),
+			'project_watchers' => array(
+				'label' => 'Project watchers',
+				'context' => CerberusContexts::CONTEXT_WORKER,
+			),
+		);
+		
+		$vars = parent::getValuesContexts($trigger);
+		
+		$vals_to_ctx = array_merge($vals, $vars);
+		asort($vals_to_ctx);
+		
+		return $vals_to_ctx;
+	}
+	
+	function getConditionExtensions() {
+		$labels = $this->getLabels();
+		
+		$labels['project_link'] = 'Jira project is linked';
+		$labels['project_watcher_count'] = 'Jira project watcher count';
+		
+		$types = array(
+			'project_jira_key' => Model_CustomField::TYPE_SINGLE_LINE,
+			'project_last_synced_at' => Model_CustomField::TYPE_DATE,
+			'project_name' => Model_CustomField::TYPE_SINGLE_LINE,
+			'project_record_url' => Model_CustomField::TYPE_URL,
+			'project_url' => Model_CustomField::TYPE_URL,
+			
+			'project_link' => null,
+			'project_watcher_count' => null,
+		);
+
+		$conditions = $this->_importLabelsTypesAsConditions($labels, $types);
+		
+		return $conditions;
+	}
+	
+	function renderConditionExtension($token, $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+
+		if(!is_null($seq))
+			$tpl->assign('namePrefix','condition'.$seq);
+		
+		switch($token) {
+			case 'project_link':
+				$contexts = Extension_DevblocksContext::getAll(false);
+				$tpl->assign('contexts', $contexts);
+				$tpl->display('devblocks:cerberusweb.core::events/condition_link.tpl');
+				break;
+				
+			case 'project_watcher_count':
+				$tpl->display('devblocks:cerberusweb.core::internal/decisions/conditions/_number.tpl');
+				break;
+		}
+
+		$tpl->clearAssign('namePrefix');
+		$tpl->clearAssign('params');
+	}
+	
+	function runConditionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$pass = true;
+		
+		switch($token) {
+			case 'project_link':
+				$not = (substr($params['oper'],0,1) == '!');
+				$oper = ltrim($params['oper'],'!');
+				
+				$from_context = null;
+				$from_context_id = null;
+
+				switch($token) {
+					case 'project_link':
+						$from_context = 'cerberusweb.contexts.jira.project';
+						@$from_context_id = $dict->project_id;
+						break;
+					default:
+						$pass = false;
+				}
+				
+				// Get links by context+id
+				
+				if(!empty($from_context) && !empty($from_context_id)) {
+					@$context_strings = $params['context_objects'];
+					$links = DAO_ContextLink::intersect($from_context, $from_context_id, $context_strings);
+					
+					// OPER: any, !any, all
+					switch($oper) {
+						case 'in':
+							$pass = (is_array($links) && !empty($links));
+							break;
+						case 'all':
+							$pass = (is_array($links) && count($links) == count($context_strings));
+							break;
+						default:
+							$pass = false;
+							break;
+					}
+					
+				} else {
+					$pass = false;
+				}
+				
+				$pass = ($not) ? !$pass : $pass;
+				break;
+				
+			case 'project_watcher_count':
+				$not = (substr($params['oper'],0,1) == '!');
+				$oper = ltrim($params['oper'],'!');
+
+				switch($token) {
+					default:
+						$value = count($dict->project_watchers);
+						break;
+				}
+				
+				switch($oper) {
+					case 'is':
+						$pass = intval($value)==intval($params['value']);
+						break;
+					case 'gt':
+						$pass = intval($value) > intval($params['value']);
+						break;
+					case 'lt':
+						$pass = intval($value) < intval($params['value']);
+						break;
+				}
+				
+				$pass = ($not) ? !$pass : $pass;
+				break;
+				
+			default:
+				$pass = false;
+				break;
+		}
+		
+		return $pass;
+	}
+	
+	function getActionExtensions() {
+		$actions =
+			array(
+				'add_watchers' => array('label' =>'Add watchers'),
+				'create_comment' => array('label' =>'Create a comment'),
+				'create_notification' => array('label' =>'Create a notification'),
+				'create_task' => array('label' =>'Create a task'),
+				'create_ticket' => array('label' =>'Create a ticket'),
+				'send_email' => array('label' => 'Send email'),
+				'set_links' => array('label' => 'Set links'),
+			)
+			+ DevblocksEventHelper::getActionCustomFieldsFromLabels($this->getLabels())
+			;
+			
+		return $actions;
+	}
+	
+	function renderActionExtension($token, $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+
+		if(!is_null($seq))
+			$tpl->assign('namePrefix','action'.$seq);
+
+		$labels = $this->getLabels($trigger);
+		$tpl->assign('token_labels', $labels);
+			
+		switch($token) {
+			case 'add_watchers':
+				DevblocksEventHelper::renderActionAddWatchers($trigger);
+				break;
+			
+			case 'create_comment':
+				DevblocksEventHelper::renderActionCreateComment($trigger);
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::renderActionCreateNotification($trigger);
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::renderActionCreateTask($trigger);
+				break;
+				
+			case 'create_ticket':
+				DevblocksEventHelper::renderActionCreateTicket($trigger);
+				break;
+				
+			case 'send_email':
+				DevblocksEventHelper::renderActionSendEmail($trigger);
+				break;
+
+			case 'set_links':
+				DevblocksEventHelper::renderActionSetLinks($trigger);
+				break;
+				
+			default:
+				if(preg_match('#set_cf_(.*?)_custom_([0-9]+)#', $token, $matches)) {
+					$field_id = $matches[2];
+					$custom_field = DAO_CustomField::get($field_id);
+					DevblocksEventHelper::renderActionSetCustomField($custom_field, $trigger);
+				}
+				break;
+		}
+		
+		$tpl->clearAssign('params');
+		$tpl->clearAssign('namePrefix');
+		$tpl->clearAssign('token_labels');
+	}
+	
+	function simulateActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		@$project_id = $dict->project_id;
+
+		if(empty($project_id))
+			return;
+		
+		switch($token) {
+			case 'add_watchers':
+				return DevblocksEventHelper::simulateActionAddWatchers($params, $dict, 'project_id');
+				break;
+			case 'create_comment':
+				return DevblocksEventHelper::simulateActionCreateComment($params, $dict, 'project_id');
+				break;
+			case 'create_notification':
+				return DevblocksEventHelper::simulateActionCreateNotification($params, $dict, 'project_id');
+				break;
+			case 'create_task':
+				return DevblocksEventHelper::simulateActionCreateTask($params, $dict, 'project_id');
+				break;
+			case 'create_ticket':
+				return DevblocksEventHelper::simulateActionCreateTicket($params, $dict);
+				break;
+			case 'send_email':
+				return DevblocksEventHelper::simulateActionSendEmail($params, $dict);
+				break;
+			case 'set_links':
+				return DevblocksEventHelper::simulateActionSetLinks($trigger, $params, $dict);
+				break;
+			default:
+				if(preg_match('#set_cf_(.*?)_custom_([0-9]+)#', $token))
+					return DevblocksEventHelper::simulateActionSetCustomField($token, $params, $dict);
+				break;
+		}
+	}
+	
+	function runActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		@$project_id = $dict->project_id;
+
+		if(empty($project_id))
+			return;
+		
+		switch($token) {
+			case 'add_watchers':
+				DevblocksEventHelper::runActionAddWatchers($params, $dict, 'project_id');
+				break;
+			
+			case 'create_comment':
+				DevblocksEventHelper::runActionCreateComment($params, $dict, 'project_id');
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::runActionCreateNotification($params, $dict, 'project_id');
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::runActionCreateTask($params, $dict, 'project_id');
+				break;
+
+			case 'create_ticket':
+				DevblocksEventHelper::runActionCreateTicket($params, $dict);
+				break;
+				
+			case 'send_email':
+				DevblocksEventHelper::runActionSendEmail($params, $dict);
+				break;
+				
+			case 'set_links':
+				DevblocksEventHelper::runActionSetLinks($trigger, $params, $dict);
+				break;
+				
+			default:
+				if(preg_match('#set_cf_(.*?)_custom_([0-9]+)#', $token))
+					return DevblocksEventHelper::runActionSetCustomField($token, $params, $dict);
+				break;
+		}
+	}
+	
+};
