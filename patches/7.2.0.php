103e4c4cd0896062efeedf93e646dc3e7abbabeb
Jeff Standen <jeff@webgroupmedia.com>
2016-08-23 13:48:35 -0700

    * [Plugins/JIRA/Performance] Added more indexes to the jira_project table to improve performance with many project records.

--- patches/7.2.0.php
+++ patches/7.2.0.php
@@ -13,8 +13,22 @@ if(!isset($tables['jira_project'])) {
 
 list($columns, $indexes) = $db->metaTable('jira_project');
 
-if(!isset($columns['last_synced_checkpoint'])) {
-	$db->ExecuteMaster("ALTER TABLE jira_project ADD COLUMN last_synced_checkpoint INT UNSIGNED NOT NULL DEFAULT 0");
+if(!isset($columns['last_synced_checkpoint']))
+	$changes[] = 'ADD COLUMN last_synced_checkpoint INT UNSIGNED NOT NULL DEFAULT 0';
+
+if(!isset($indexes['last_synced_at']))
+	$changes[] = 'ADD INDEX (last_synced_at)';
+
+if(!isset($indexes['is_sync']))
+	$changes[] = 'ADD INDEX (is_sync)';
+
+if(!isset($indexes['jira_id']))
+	$changes[] = 'ADD INDEX (jira_id)';
+
+if(!empty($changes)) {
+	$sql = sprintf("ALTER TABLE jira_project %s", implode(', ', $changes));
+	$db->ExecuteMaster($sql) or die("[MySQL Error] " . $db->ErrorMsgMaster());
 }
+
 
 return TRUE;
