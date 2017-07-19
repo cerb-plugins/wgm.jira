b921c980e40d27ec4485041eb2d58ae8648c4941
Jeff Standen <jeff@webgroupmedia.com>
2017-07-19 01:19:11 -0700

    8.0 compatibility

--- api/events/abstract_event_jira_issue.php
+++ api/events/abstract_event_jira_issue.php
@@ -183,7 +183,7 @@ abstract class AbstractEvent_JiraIssue extends Extension_DevblocksEvent {
 				'context' => CerberusContexts::CONTEXT_BEHAVIOR,
 			),
 			'behavior_bot_id' => array(
-				'label' => 'Behavior',
+				'label' => 'Bot',
 				'context' => CerberusContexts::CONTEXT_BOT,
 			),
 			'issue_id' => array(
